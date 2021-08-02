baf0538945421ee5301c0a02ca59c5083608ff96
nhanluongoe <nhanlt18@gmail.com>
2021-08-03 00:44:30 +0700

    question: add a question and rename file

---
+++ questions/question_1.sql
@@ -0,0 +1,10 @@
+-- How can you output a list of all members, including the individual who recommended them (if any), without using any joins? Ensure that there are no duplicates in the list, and that each firstname + surname pairing is formatted as a column and ordered.
+
+SELECT DISTINCT mems.firstname || ' ' || mems.surname AS member,
+  (
+    SELECT recs.firstname || ' ' || recs.surname as recommender
+    FROM cd.members recs
+    WHERE recs.memid = mems.recommendedby
+  )
+FROM cd.members mems
+ORDER BY member;
