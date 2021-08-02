9288b03f21b270a38521b61a00b91a98c67d728f
nhanluongoe <nhanlt18@gmail.com>
2021-08-03 01:05:35 +0700

    question: add question 3

---
+++ questions/question_3.sql
@@ -0,0 +1,18 @@
+SELECT member,
+  facility,
+  cost
+FROM (
+    SELECT mems.firstname || ' ' || mems.surname AS member,
+      facs.name AS facility,
+      CASE
+        WHEN mems.memid = 0 THEN bks.slots * guestcost
+        ELSE bks.slots * membercost
+      END as cost
+    FROM cd.members mems
+      INNER JOIN cd.bookings bks ON mems.memid = bks.memid
+      INNER JOIN cd.facilities facs ON bks.facid = facs.facid
+    WHERE bks.starttime >= '2012-09-14'
+      AND bks.starttime < '2012-09-15'
+  )
+WHERE cost > 30
+ORDER BY cost DESC;
