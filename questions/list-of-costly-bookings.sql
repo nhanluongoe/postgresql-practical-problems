4141c28a685e088729b0b2a107b61c7117cc29ac
nhanluongoe <nhanlt18@gmail.com>
2021-08-03 00:27:46 +0700

    refactor: add spaces

--- questions/list-of-costly-bookings.sql
+++ questions/list-of-costly-bookings.sql
@@ -1,4 +1,5 @@
 -- How can you produce a list of bookings on the day of 2012-09-14 which will cost the member (or guest) more than $30? Remember that guests have different costs to members (the listed costs are per half-hour 'slot'), and the guest user is always ID 0. Include in your output the name of the facility, the name of the member formatted as a single column, and the cost. Order by descending cost, and do not use any subqueries.
+
 SELECT mems.firstname || ' ' || mems.surname AS member,
        facs.name AS facility,
        CASE
