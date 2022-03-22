61f9dc40e81d38445230609ceb08704a34153419
nhanluongoe <nhanlt18@gmail.com>
2022-03-22 17:26:28 +0700

    question: update a row based on the contents of another row

---
+++ modifying-data/question_6.sql
@@ -0,0 +1,13 @@
+-- We want to alter the price of the second tennis court so that it costs 10% more than the first one. Try to do this without using constant values for the prices, so that we can reuse the statement if we want to.
+UPDATE cd.facilities
+SET membercost = (
+    SELECT membercost * 1.1
+    FROM cd.facilities
+    WHERE facid = 0
+  ),
+  guestcost = (
+    SELECT guestcost * 1.1
+    FROM cd.facilities
+    WHERE facid = 0
+  )
+WHERE facid = 1
