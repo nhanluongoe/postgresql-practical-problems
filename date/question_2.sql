cfe8e03f762b55a90fb294cbdbbc3493a1955c3f
nhanluongoe <nhanlt18@gmail.com>
2022-05-09 15:23:50 +0700

    question: subtract timestamps from each other

---
+++ date/question_2.sql
@@ -0,0 +1,3 @@
+-- Find the result of subtracting the timestamp '2012-07-30 01:00:00' from the timestamp '2012-08-31 01:00:00'
+
+select timestamp '2012-08-31 01:00:00' - timestamp '2012-07-30 01:00:00' as interval
